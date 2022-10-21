1cbe6b65fa9d0ed90cffd4cf266d191626b8bf83
Sam Bracey <bracey.sam@gmail.com>
2022-10-21 11:16:21 +1100

    Cleanup

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const gridContainer = document.getElementById("etchScreen");
 
-let size = 5;
+//Get grid size
+const btn = document.querySelector("#gridSize");
+btn.onclick = () => prompt("How many cells across/down?", "16");
+let size = 16;
 
-// insert specified divs
-for (let i = 0; i < size * size; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("class", "square div" + i);
-  gridContainer.appendChild(div);
+createGrid();
+
+// Create the grid with specified divs
+function createGrid() {
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "square div" + i);
+    gridContainer.appendChild(div);
+  }
+  gridContainer.style.gridTemplateColumns =
+    "repeat(" + size + ", minmax(0, 1fr))";
 }
-
-gridContainer.style.gridTemplateColumns =
-  "repeat(" + size + ", minmax(0, 1fr))";
